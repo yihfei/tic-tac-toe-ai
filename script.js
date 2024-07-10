4cfd7d49ce03a1958909257145f8ec944ddc3dee
yihfei <lim.yih.fei@gmail.com>
2024-07-10 15:19:58 +0800

    Add minimax logic

--- script.js
+++ script.js
@@ -19,8 +19,12 @@ const gameBoard = (() => {
     const setBoard = (player, index) => {
         board[index] = player.getSign();
     } 
+
+    const clearBoard = (index) => {
+        board[index] = '';
+    }
  
-    const clearBoard = () => { 
+    const clearEntireBoard = () => { 
         board = [
         '', '', '',
         '', '', '',
@@ -28,9 +32,18 @@ const gameBoard = (() => {
         ];
     }
 
+    const getAvailableCells = () => {
+        return board.map((cell, index) => cell == '' ? index : null)
+            .filter(index => index !== null);
+    }
+
     const checkWin = (player) => {
         return WINNING_COMBINATIONS.some(combination =>
             combination.every(index => gameBoard[index] == player.getSign()));
+    }
+
+    const checkDraw = () => {
+        return board.every(cell => cell != '');
     }
 
     return {setBoard, clearBoard, checkWin};
@@ -48,5 +61,71 @@ const gameController = (() => {
     const human = Player("X");
     const ai = Player("O");
 
+    const getHumanPlayer = () => human;
+    const getAiPlayer = () => ai;
+    
+    return {getHumanPlayer, getAiPlayer};
+    
 })();
+
+
+function minimax(player) {
+    const availableCells = gameBoard.getAvailableCells();
+
+    // Check win
+    if (gameBoard.checkWin(gameController.getHumanPlayer())) {
+        return { score : 1};
+    } else if (gameBoard.checkWin(gameController.getAiPlayer())) {
+        return { score : -1};
+    } else if (checkDraw()){
+        return { score : 0 };
+    }
+
+    let moves = [];
+
+    for (let cell of availableCells) {
+        let move = {}; // object required to store score AND moves
+        move.index = cell;
+
+        gameBoard.setBoard(player, cell);
+
+        if (player == gameController.getHumanPlayer()) {
+            let result = gameController.getAiPlayer();
+            move.score = result.score;
+        } else {
+            let result = gameController.getHumanPlayer();
+            move.score = result.score;
+        }
+
+        // Clear current move before trying next cell
+        gameBoard.clearBoard(cell);
+
+        moves.push(move);
+    }
+
+    let BestMove;
+
+    // maximise score for humans
+    if (player == gameController.getHumanPlayer()) {
+        let bestScore = -Infinity;
+        for (let move of moves) {
+            if (move.score > bestScore) {
+                bestScore = move.score;
+                bestMove = move;
+            }
+        }
+    }
+
+    if (player == gameController.getAiPlayer()) {
+        let bestScore = Infinity;
+        for (let move of moves) {
+            if (move.score < bestScore) {
+                bestScore = move.score;
+                bestMove = move;
+            }
+        }
+    }
+
+    return bestMove;
+}
 
